b4b98dbaf3baa17c47f4a3db7f83ea4bfdae0de4
Nonnie Martin <martin.nonnie@gmail.com>
2017-09-11 09:27:05 -0600

    Fixing bad connection

---
+++ excercises/Functions/app.js
@@ -0,0 +1,35 @@
+function add(num1, num2){  
+  return num1 + num2;
+}
+var total = add(3,5); //total = 8  
+
+function Math.max(num1, num2, num3){
+     return Math.max;
+}
+ 
+
+
+
+
+function isEven(n) {
+  n = Number(n);
+  return n === 0 || !!(n && !(n%2));
+}
+
+function isOdd(n) {
+  return isEven(Number(n) + 1);
+}
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
